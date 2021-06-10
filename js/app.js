b1e017738dc7cc9f884025c0f494257e63f75f7f
lgcarlinf <lgcarlinf@unac.edu.pe>
2021-06-10 07:55:57 -0500

    changes

--- js/app.js
+++ js/app.js
@@ -1,18 +1,30 @@
 function addrow()
 {
-    var table = document.getElementById("students");
-    var row = table.insertRow(-1);
-    var cell1 = row.insertCell(0);
-    var cell2 = row.insertCell(1);
-    var cell3 = row.insertCell(2);
-    var cell4 = row.insertCell(3);
+    let table = document.getElementById("students");
+    let row = table.insertRow(-1);
+    let cell1 = row.insertCell(0);
+    let cell2 = row.insertCell(1);
+    let cell3 = row.insertCell(2);
+    let cell4 = row.insertCell(3);
     cell1.innerHTML = "";
     cell2.innerHTML = "";
     cell3.innerHTML = "";
     cell4.innerHTML = "";
 }
+
 function delrow()
 {
-    var table = document.getElementById("students");
+    let table = document.getElementById("students");
     table.deleteRow(-1)
 }
+
+let buttonAdd = document.querySelector('#insert');
+let buttonDel= document.querySelector('#del');
+
+buttonAdd.addEventListener('click', () =>{
+    addrow()
+})
+
+buttonDel.addEventListener('click', () =>{
+    delrow()
+})
